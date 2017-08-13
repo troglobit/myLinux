3793a19d7dd7948e559a587bc476919c8e3d99e3
Joachim Nilsson <troglobit@gmail.com>
2017-08-13 21:27:16 +0200

    ramdisk.mk: Create .img version of initramfs as well, depend on .cpio
    Signed-off-by: Joachim Nilsson <troglobit@gmail.com>

--- ramdisk.mk
+++ ramdisk.mk
@@ -3,7 +3,7 @@
 include core.mk
 
 # images/initramfs-$(KERNELRELEASE).img images/initramfs-$(KERNELRELEASE).uImage
-RAMDISK        := images/initramfs-$(KERNELRELEASE).gz
+RAMDISK := images/initramfs-$(KERNELRELEASE).gz images/initramfs-$(KERNELRELEASE).img
 
 ramdisk: $(RAMDISK)
 	@ln -sf `basename $(RAMDISK)` images/initramfs.gz
@@ -18,8 +18,8 @@ images/initramfs-$(KERNELRELEASE).gz: images/initramfs-$(KERNELRELEASE).cpio
 images/initramfs-$(KERNELRELEASE).lzo: images/initramfs-$(KERNELRELEASE).cpio
 	@lzop -9 -o $@ $<
 
-images/initramfs-$(KERNELRELEASE).img: images/initramfs-$(KERNELRELEASE).gz
-	@mkimage -T ramdisk -A arm -C none -d $< $@
+images/initramfs-$(KERNELRELEASE).img: images/initramfs-$(KERNELRELEASE).cpio
+	@mkimage -T ramdisk -A arm -C none -n uInitrd -d $< $@
 
 images/initramfs-$(KERNELRELEASE).cpio: $(ROMFS)/etc/version
 	@(cat initramfs.devnodes; \
