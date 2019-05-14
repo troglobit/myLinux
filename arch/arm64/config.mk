d2a4bb64e396792971d68b21a3c68540cecd7e3e
Joachim Nilsson <troglobit@gmail.com>
2019-05-14 08:23:02 +0200

    Default to two CPUs for Aarch64
    Signed-off-by: Joachim Nilsson <troglobit@gmail.com>

--- arch/arm64/config.mk
+++ arch/arm64/config.mk
@@ -9,7 +9,7 @@ QEMU_MACH     := virt
 QEMU_NIC      := rtl8139
 QEMU_SCSI     := virtio-scsi-device
 QEMU_9P       := virtio-9p-device
-QEMU_EXTRA    := -cpu cortex-a57
+QEMU_EXTRA    := -cpu cortex-a57 -smp 2
 # freescale/fsl-ls2080a-simu.dtb # freescale/fsl-ls2080a-rdb.dtb
 #QEMU_DTB      := arm/vexpress-v2f-1xv7-ca53x2.dtb
 
