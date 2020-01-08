e312a552072af463580fb76864e525f21655911b
Joachim Nilsson <troglobit@gmail.com>
2020-01-08 11:50:48 +0100

    Remove dangerous (and mostly unused) KERNELRELEASE variable
    Depending on *when* make evaluated the variable, make could suddenly
    start executing "make -C linux-x.y.c kernelrelease" inside a  half-
    configured kernel package, causing all kinds of meltdown.
    
    Only ramdisk.mk used the value, for versioning the files, as well as
    a commented-out modules rule, which is also removed in this commit.
    
    Signed-off-by: Joachim Nilsson <troglobit@gmail.com>

--- arch/ramdisk.mk
+++ arch/ramdisk.mk
@@ -2,28 +2,28 @@
 
 include $(ROOTDIR)/system/core.mk
 
-# images/initramfs-$(KERNELRELEASE).img images/initramfs-$(KERNELRELEASE).uImage
-RAMDISK := images/initramfs-$(KERNELRELEASE).gz
-RAMIMG  := images/initramfs-$(KERNELRELEASE).img
+# images/initramfs.img images/initramfs.uImage
+RAMDISK := images/initramfs.gz
+RAMIMG  := images/initramfs.img
 
 ramdisk: $(RAMDISK) $(RAMIMG)
 	@ln -f $(RAMDISK) images/initramfs.gz
 	@ln -f $(RAMIMG)  images/initramfs.img
 
 # http://free-electrons.com/blog/uncompressed-linux-kernel-on-arm/
-images/initramfs-$(KERNELRELEASE).uImage:
+images/initramfs.uImage:
 	@cp -p $(KERNEL_MODULES)/build/arch/arm/boot/uImage $@
 
-images/initramfs-$(KERNELRELEASE).gz: images/initramfs-$(KERNELRELEASE).cpio
+images/initramfs.gz: images/initramfs.cpio
 	@gzip -9 < $< >$@
 
-images/initramfs-$(KERNELRELEASE).lzo: images/initramfs-$(KERNELRELEASE).cpio
+images/initramfs.lzo: images/initramfs.cpio
 	@lzop -9 -o $@ $<
 
-images/initramfs-$(KERNELRELEASE).img: images/initramfs-$(KERNELRELEASE).cpio
+images/initramfs.img: images/initramfs.cpio
 	@mkimage -T ramdisk -A $(ARCH) -C none -n uInitrd -d $< $@
 
-images/initramfs-$(KERNELRELEASE).cpio: $(ROMFS)/etc/version
+images/initramfs.cpio: $(ROMFS)/etc/version
 	@(cat initramfs.devnodes; \
 	  sh $(KERNEL_MODULES)/build/usr/gen_initramfs_list.sh -u squash -g squash $(ROMFS)) \
 	   | $(KERNEL_MODULES)/build/usr/gen_init_cpio - >$@
