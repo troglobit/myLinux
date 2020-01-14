e7254f7266c35926498bd81eb8d555ab2c4fdc26
Joachim Nilsson <troglobit@gmail.com>
2020-01-14 19:52:33 +0100

    Sync arm with latest changes, leaner set of base packages
    Signed-off-by: Joachim Nilsson <troglobit@gmail.com>

--- arch/arm/config.mk
+++ arch/arm/config.mk
@@ -5,7 +5,6 @@ QEMU_ARCH     := arm
 QEMU_MACH     := versatilepb
 QEMU_NIC      := smc91c111
 QEMU_SCSI     := virtio-scsi-pci
-QEMU_9P       := virtio-9p-pci
 QEMU_EXTRA    := -rtc base=utc,clock=rt
 QEMU_DTB      := versatile-pb.dtb
 
