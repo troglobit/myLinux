9f40b733124795ccb0b9e9ae1e292790c5892ee9
Joachim Nilsson <troglobit@gmail.com>
2020-01-18 18:54:35 +0100

    ppc: Add missing pieces to kernel .config puzzle and make ppc boot
    Signed-off-by: Joachim Nilsson <troglobit@gmail.com>

--- arch/ppc/config.mk
+++ arch/ppc/config.mk
@@ -1,10 +1,10 @@
 .EXPORT_ALL_VARIABLES:
 
 KERNEL_IMG    := zImage
-QEMU_ARCH     := ppc
+QEMU_ARCH     := ppc64
 QEMU_MACH     := ppce500
 QEMU_NIC      := rtl8139
 QEMU_SCSI     := virtio-scsi-pci
 QEMU_9P       := virtio-9p-pci
-QEMU_EXTRA    :=
+QEMU_EXTRA    := -cpu e5500
 QEMU_DTB      :=
