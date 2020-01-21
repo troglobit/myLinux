36e0fe6a9626912bb9ee17ff20df2eb7e8a7172c
Joachim Nilsson <troglobit@gmail.com>
2020-01-21 08:56:14 +0100

    ppc: Enable SMP and set -rtc clock=host
    Signed-off-by: Joachim Nilsson <troglobit@gmail.com>

--- arch/ppc/config.mk
+++ arch/ppc/config.mk
@@ -2,9 +2,9 @@
 
 KERNEL_IMG    := zImage
 QEMU_ARCH     := ppc64
-QEMU_MACH     := ppce500
+QEMU_MACH     := ppce500 -smp 2
 QEMU_NIC      := rtl8139
 QEMU_SCSI     := virtio-scsi-pci
 QEMU_9P       := virtio-9p-pci
-QEMU_EXTRA    := -cpu e5500
+QEMU_EXTRA    := -cpu e5500 -rtc clock=host
 QEMU_DTB      :=
