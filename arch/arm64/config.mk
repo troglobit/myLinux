80da5d4a912a54c327b2435cb1855f77b0d5cbc8
Joachim Nilsson <troglobit@gmail.com>
2020-01-30 18:43:29 +0100

    Allow override of Qemu 9P host dir, kernel appends, /mnt disk
    Signed-off-by: Joachim Nilsson <troglobit@gmail.com>

--- arch/arm64/config.mk
+++ arch/arm64/config.mk
@@ -1,12 +1,9 @@
 .EXPORT_ALL_VARIABLES:
 
 QEMU_ARCH     := aarch64
-# vexpress-a15
 QEMU_MACH     := virt
-# lan9118
 QEMU_NIC      := rtl8139
 QEMU_SCSI     := virtio-scsi-device
-QEMU_9P       := virtio-9p-device
 QEMU_EXTRA    := -cpu cortex-a57 -smp 2
 # freescale/fsl-ls2080a-simu.dtb # freescale/fsl-ls2080a-rdb.dtb
 #QEMU_DTB      := arm/vexpress-v2f-1xv7-ca53x2.dtb
--- arch/ppc/config.mk
+++ arch/ppc/config.mk
@@ -4,6 +4,5 @@ QEMU_ARCH     := ppc64
 QEMU_MACH     := ppce500 -smp 2
 QEMU_NIC      := rtl8139
 QEMU_SCSI     := virtio-scsi-pci
-QEMU_9P       := virtio-9p-pci
 QEMU_EXTRA    := -cpu e5500 -rtc clock=host
 QEMU_DTB      :=
