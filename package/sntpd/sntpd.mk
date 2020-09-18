ed31e0ecdd455495d28834c753b3a6b6b4080665
Joachim Wiberg <troglobit@gmail.com>
2020-09-18 22:17:02 +0200

    package/snptd: new package
    Migrate from legacy/packages and adapt to Buildroot build system.
    
    Signed-off-by: Joachim Wiberg <troglobit@gmail.com>

---
+++ package/sntpd/sntpd.mk
@@ -0,0 +1,19 @@
+################################################################################
+#
+# sntpd
+#
+################################################################################
+SNTPD_VERSION = 3.0
+SNTPD_SOURCE = sntpd-$(SNTPD_VERSION).tar.gz
+SNTPD_SITE = https://github.com/troglobit/sntpd/releases/download/v$(SNTPD_VERSION)
+SNTPD_LICENSE = GPL-2.0+
+
+define SNTPD_INSTALL_FINIT_SVC
+	$(INSTALL) -D -m 0644 $(BR2_EXTERNAL_MYLINUX_PATH)/package/sntpd/sntpd.svc \
+		$(TARGET_DIR)/etc/finit.d/available/sntpd.conf
+	ln -sf /etc/finit.d/available/sntpd.conf $(FINIT_D)/enabled/sntpd.conf
+endef
+
+SNTPD_POST_INSTALL_TARGET_HOOKS += SNTPD_INSTALL_FINIT_SVC
+
+$(eval $(autotools-package))
