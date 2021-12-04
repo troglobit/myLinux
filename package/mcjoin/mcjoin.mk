ba03801f1401f84435511d3cb705590c0468a493
Joachim Wiberg <troglobit@gmail.com>
2021-12-04 09:52:00 +0100

    package/mcjoin: new package
    Import mcjoin from NetBox.
    
    Signed-off-by: Joachim Wiberg <troglobit@gmail.com>

---
+++ package/mcjoin/mcjoin.mk
@@ -0,0 +1,12 @@
+################################################################################
+#
+# mcjoin
+#
+################################################################################
+
+MCJOIN_VERSION = 2.9
+MCJOIN_SITE    = https://github.com/troglobit/mcjoin/releases/download/v$(MCJOIN_VERSION)
+MCJOIN_LICENSE = ISC
+MCJOIN_LICENSE_FILES = LICENSE
+
+$(eval $(autotools-package))
