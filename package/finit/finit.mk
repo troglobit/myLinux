efb9ecce602df0fcf731be786672e75798e16a38
Joachim Wiberg <troglobit@gmail.com>
2021-12-04 09:32:19 +0100

    package/finit: new package
    Import packaging for Finit from NetBox, including patches and skeleton.
    
    Signed-off-by: Joachim Wiberg <troglobit@gmail.com>

---
+++ package/finit/finit.mk
@@ -0,0 +1,89 @@
+################################################################################
+#
+# finit
+#
+################################################################################
+
+FINIT_VERSION = 4.1
+FINIT_SOURCE = finit-$(FINIT_VERSION).tar.gz
+FINIT_SITE = https://github.com/troglobit/finit/releases/download/$(FINIT_VERSION)
+FINIT_LICENSE = MIT
+FINIT_LICENSE_FILES = LICENSE
+FINIT_INSTALL_STAGING = YES
+FINIT_DEPENDENCIES = host-pkgconf libite libuev
+FINIT_D = $(TARGET_DIR)/etc/finit.d
+
+# Create configure script using autoreconf when building from git
+#FINIT_VERSION = 099672f
+#FINIT_SITE = git://github.com/troglobit/finit.git
+#FINIT_AUTORECONF = YES
+#FINIT_DEPENDENCIES += host-automake host-autoconf host-libtool
+
+# Buildroot defaults to /usr for both prefix and exec-prefix, this we
+# must override because we want to install into /sbin and /bin for the
+# finit and initctl programs, respectively.  The expected plugin path is
+# /lib/finit/ and scripts in /libexec, both are set by --exec-prefix.
+# The localstatedir is set to the correct system path by Buildroot, so
+# no override necessary there.
+FINIT_CONF_OPTS =					\
+	--prefix=/usr					\
+	--exec-prefix=					\
+	--disable-doc					\
+	--disable-contrib				\
+	--with-group=$(BR2_PACKAGE_FINIT_INITCTL_GROUP)
+
+# Disable/Enable features
+ifeq ($(BR2_PACKAGE_FINIT_KEVENTD),y)
+FINIT_CONF_OPTS += --with-keventd
+else
+FINIT_CONF_OPTS += --without-keventd
+endif
+
+ifeq ($(BR2_PACKAGE_FINIT_WATCHDOG),y)
+FINIT_CONF_OPTS += --with-watchdog=$(BR2_PACKAGE_FINIT_WATCHDOG_DEV)
+else
+FINIT_CONF_OPTS += --without-watchdog
+endif
+
+ifeq ($(BR2_PACKAGE_FINIT_PLUGIN_HOTPLUG),y)
+FINIT_CONF_OPTS += --enable-hotplug-plugin
+else
+FINIT_CONF_OPTS += --disable-hotplug-plugin
+endif
+
+ifeq ($(BR2_PACKAGE_FINIT_PLUGIN_RTC),y)
+FINIT_CONF_OPTS += --enable-rtc-plugin
+else
+FINIT_CONF_OPTS += --disable-rtc-plugin
+endif
+
+ifeq ($(BR2_PACKAGE_FINIT_PLUGIN_URANDOM),y)
+FINIT_CONF_OPTS += --enable-urandom-plugin
+else
+FINIT_CONF_OPTS += --disable-urandom-plugin
+endif
+
+ifneq ($(SKELETON_INIT_COMMON_HOSTNAME),)
+FINIT_CONF_OPTS += --with-hostname="$(SKELETON_INIT_COMMON_HOSTNAME)"
+endif
+
+# Disable/Enable features depending on other packages
+ifeq ($(BR2_PACKAGE_ALSA_UTILS),y)
+FINIT_CONF_OPTS += --enable-alsa-utils-plugin
+else
+FINIT_CONF_OPTS += --disable-alsa-utils-plugin
+endif
+
+ifeq ($(BR2_PACKAGE_DBUS),y)
+FINIT_CONF_OPTS += --enable-dbus-plugin
+else
+FINIT_CONF_OPTS += --disable-dbus-plugin
+endif
+
+ifeq ($(BR2_PACKAGE_XORG7),y)
+FINIT_CONF_OPTS += --enable-x11-common-plugin
+else
+FINIT_CONF_OPTS += --disable-x11-common-plugin
+endif
+
+$(eval $(autotools-package))
---
+++ package/skeleton-init-finit/skeleton-init-finit.mk
@@ -0,0 +1,108 @@
+################################################################################
+#
+# skeleton-init-finit
+#
+################################################################################
+
+# The skeleton can't depend on the toolchain, since all packages depends on the
+# skeleton and the toolchain is a target package, as is skeleton.
+# Hence, skeleton would depends on the toolchain and the toolchain would depend
+# on skeleton.
+SKELETON_INIT_FINIT_ADD_TOOLCHAIN_DEPENDENCY = NO
+SKELETON_INIT_FINIT_ADD_SKELETON_DEPENDENCY = NO
+SKELETON_INIT_FINIT_TMPFILE := $(shell mktemp)
+SKELETON_INIT_FINIT_DEPENDENCIES = skeleton-init-common
+
+# Enable when BR2_INIT_FINT
+#SKELETON_INIT_FINIT_PROVIDES = skeleton
+
+# Prefer Finit built-in getty unless options are set, squash zero baudrate
+define SKELETON_INIT_FINIT_GETTY
+	if [ -z "$(SYSTEM_GETTY_OPTIONS)" ]; then \
+		if [ $(SYSTEM_GETTY_BAUDRATE) -eq 0 ]; then \
+			SYSTEM_GETTY_BAUDRATE=""; \
+		fi; \
+		echo "tty [12345789] $(SYSTEM_GETTY_PORT) $(SYSTEM_GETTY_BAUDRATE) $(SYSTEM_GETTY_TERM) noclear"; \
+	else \
+		echo "tty [12345789] /sbin/getty -L $(SYSTEM_GETTY_OPTIONS) $(SYSTEM_GETTY_BAUDRATE) $(SYSTEM_GETTY_PORT) $(SYSTEM_GETTY_TERM)"; \
+	fi
+endef
+
+define SKELETON_INIT_FINIT_SET_GENERIC_GETTY
+	$(SKELETON_INIT_FINIT_GETTY) > $(SKELETON_INIT_FINIT_TMPFILE)
+	grep -qxF "`cat $(SKELETON_INIT_FINIT_TMPFILE)`" $(FINIT_D)/available/getty.conf \
+		|| cat $(SKELETON_INIT_FINIT_TMPFILE) >> $(FINIT_D)/available/getty.conf
+	rm $(SKELETON_INIT_FINIT_TMPFILE)
+	ln -sf ../available/getty.conf $(FINIT_D)/enabled/getty.conf
+endef
+SKELETON_INIT_FINIT_TARGET_FINALIZE_HOOKS += SKELETON_INIT_FINIT_SET_GENERIC_GETTY
+
+# Dropbear SSH
+ifeq ($(BR2_PACKAGE_DROPBEAR),y)
+define SKELETON_INIT_FINIT_SET_DROPBEAR
+	ln -sf ../available/dropbear.conf $(FINIT_D)/enabled/dropbear.conf
+endef
+SKELETON_INIT_FINIT_TARGET_FINALIZE_HOOKS += SKELETON_INIT_FINIT_SET_DROPBEAR
+endif
+
+# Enable Busybox syslogd unless sysklogd is enabled
+ifeq ($(BR2_PACKAGE_SYSKLOGD),y)
+define SKELETON_INIT_FINIT_SET_SYSLOGD
+	ln -sf ../available/sysklogd.conf $(FINIT_D)/enabled/sysklogd.conf
+endef
+else
+define SKELETON_INIT_FINIT_SET_SYSLOGD
+	ln -sf ../available/syslogd.conf $(FINIT_D)/enabled/syslogd.conf
+endef
+endif
+SKELETON_INIT_FINIT_TARGET_FINALIZE_HOOKS += SKELETON_INIT_FINIT_SET_SYSLOGD
+
+# SSDP Responder
+ifeq ($(BR2_PACKAGE_SSDP_RESPONDER),y)
+define SKELETON_INIT_FINIT_SET_SSDP_RESPONDER
+	ln -sf ../available/ssdp-responder.conf $(FINIT_D)/enabled/ssdp-responder.conf
+endef
+SKELETON_INIT_FINIT_TARGET_FINALIZE_HOOKS += SKELETON_INIT_FINIT_SET_SSDP_RESPONDER
+endif
+
+# Watchdogd
+ifeq ($(BR2_PACKAGE_WATCHDOGD),y)
+define SKELETON_INIT_FINIT_SET_WATCHDOGD
+	ln -sf ../available/watchdogd.conf $(FINIT_D)/enabled/watchdogd.conf
+endef
+SKELETON_INIT_FINIT_TARGET_FINALIZE_HOOKS += SKELETON_INIT_FINIT_SET_WATCHDOGD
+endif
+
+# Workaround, should be in ifupdown-scripts package
+ifeq ($(BR2_PACKAGE_IFUPDOWN_SCRIPTS),y)
+define SKELETON_INIT_FINIT_IFUPDOWN_WORKAROUND
+	$(IFUPDOWN_SCRIPTS_PREAMBLE)
+	$(IFUPDOWN_SCRIPTS_LOCALHOST)
+        $(IFUPDOWN_SCRIPTS_DHCP)
+endef
+SKELETON_INIT_FINIT_TARGET_FINALIZE_HOOKS += SKELETON_INIT_FINIT_IFUPDOWN_WORKAROUND
+endif
+
+
+ifeq ($(BR2_TARGET_GENERIC_REMOUNT_ROOTFS_RW),y)
+# Uncomment /dev/root entry in fstab to allow Finit to remount it rw
+define SKELETON_INIT_FINIT_ROOT_RO_OR_RW
+	$(SED) '\:^#[[:blank:]]*/dev/root[[:blank:]]:s/^# //' $(TARGET_DIR)/etc/fstab
+endef
+else
+# Comment out /dev/root entry to prevent Finit from remounting it rw
+define SKELETON_INIT_FINIT_ROOT_RO_OR_RW
+	$(SED) '\:^/dev/root[[:blank:]]:s/^/# /' $(TARGET_DIR)/etc/fstab
+endef
+endif
+
+define SKELETON_INIT_FINIT_INSTALL_TARGET_CMDS
+	$(call SYSTEM_RSYNC,$(SKELETON_INIT_FINIT_PKGDIR)/skeleton,$(TARGET_DIR))
+	mkdir -p $(TARGET_DIR)/home
+	mkdir -p $(TARGET_DIR)/srv
+	mkdir -p $(TARGET_DIR)/var
+	[ -L $(TARGET_DIR)/var/run ] || ln -s ../run $(TARGET_DIR)/var/run
+	$(SKELETON_INIT_FINIT_ROOT_RO_OR_RW)
+endef
+
+$(eval $(generic-package))
