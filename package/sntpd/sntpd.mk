7ed27dab0639ae208eeb564d46686bed03c992c0
Joachim Wiberg <troglobit@gmail.com>
2021-12-20 10:19:39 +0100

    package/sntpd: fix missing license file, spellcheck sha255 -> sha256
    Signed-off-by: Joachim Wiberg <troglobit@gmail.com>

--- package/sntpd/sntpd.mk
+++ package/sntpd/sntpd.mk
@@ -7,6 +7,7 @@ SNTPD_VERSION = 3.0
 SNTPD_SOURCE = sntpd-$(SNTPD_VERSION).tar.gz
 SNTPD_SITE = https://github.com/troglobit/sntpd/releases/download/v$(SNTPD_VERSION)
 SNTPD_LICENSE = GPL-2.0+
+SNTPD_LICENSE_FILES = COPYING
 
 define SNTPD_INSTALL_FINIT_SVC
 	$(INSTALL) -D -m 0644 $(BR2_EXTERNAL_MYLINUX_PATH)/package/sntpd/sntpd.svc \
