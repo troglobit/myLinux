c6e3ff87f896d62b91c01f740f0873db9ac26bdd
Joachim Wiberg <troglobit@gmail.com>
2021-12-20 07:49:18 +0100

    package/uftpd: restore and upgrde package in new buildroot suit
    Signed-off-by: Joachim Wiberg <troglobit@gmail.com>

---
+++ package/uftpd/uftpd.mk
@@ -0,0 +1,21 @@
+################################################################################
+#
+# uftpd
+#
+################################################################################
+
+UFTPD_VERSION = 2.15
+UFTPD_SITE = https://github.com/troglobit/uftpd/releases/download/v$(UFTPD_VERSION)
+UFTPD_LICENSE = ISC
+UFTPD_LICENSE_FILES = LICENSE
+
+define UFTPD_INSTALL_FINIT_SVC
+	$(INSTALL) -D -m 0644 $(BR2_EXTERNAL_MYLINUX_PATH)/package/uftpd/uftpd.def \
+		$(TARGET_DIR)/etc/default/uftpd
+	$(INSTALL) -D -m 0644 $(BR2_EXTERNAL_MYLINUX_PATH)/package/uftpd/uftpd.svc \
+		$(TARGET_DIR)/etc/finit.d/available/uftpd.conf
+endef
+
+UFTPD_POST_INSTALL_TARGET_HOOKS += UFTPD_INSTALL_FINIT_SVC
+
+$(eval $(autotools-package))
