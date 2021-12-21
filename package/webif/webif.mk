0e1867e3e1658c26dc49143ca126d0ab171a4a9d
Joachim Wiberg <troglobit@gmail.com>
2021-12-21 13:21:08 +0100

    package/webif.mk: new package
    Imported from Buildroot gf5e5aeb, where it was removed in Nov 2011.
    
    We run httpd as www-data user to prevent haserl from casting an
    Exception since the CGI scripts run with #!/usr/bin/webif-page
    as interpreter, not /usr/bin/haserl.
    
    Included is an untested S50webif SysV init script.  Untested because
    myLinux use Finit by default, which is declared in webif.svc.
    
    The favicon.ico is from troglobit.com
    
    Signed-off-by: Joachim Wiberg <troglobit@gmail.com>

---
+++ package/webif/webif.mk
@@ -0,0 +1,37 @@
+#############################################################
+#
+# webif
+#
+#############################################################
+
+WEBIF_VERSION = 0.2
+WEBIF_LICENSE = GPL-2.0
+WEBIF_SITE_METHOD = local
+WEBIF_SITE = $(BR2_EXTERNAL_MYLINUX_PATH)/src/webif
+
+define WEBIF_BUILD_CMDS
+        $(TARGET_MAKE_ENV) $(TARGET_CONFIGURE_OPTS) $(MAKE) -C $(@D) \
+                LDLIBS="$(TARGET_LDFLAGS)"
+endef
+
+define WEBIF_INSTALL_TARGET_CMDS
+        $(TARGET_MAKE_ENV) $(TARGET_CONFIGURE_OPTS) $(MAKE) -C $(@D) \
+                DESTDIR="$(TARGET_DIR)" install
+	$(INSTALL) $(BR2_EXTERNAL_MYLINUX_PATH)/package/webif/httpd.conf \
+		$(TARGET_DIR)/etc/httpd.conf
+endef
+
+define WEBIF_INSTALL_INIT_SYSV
+	$(INSTALL) -m 755 -D $(BR2_EXTERNAL_MYLINUX_PATH)/package/webif/S50webif \
+		$(TARGET_DIR)/etc/init.d/S50webif
+endef
+
+define WEBIF_INSTALL_FINIT_SVC
+	$(INSTALL) -D -m 0644 $(BR2_EXTERNAL_MYLINUX_PATH)/package/webif/webif.svc \
+		$(TARGET_DIR)/etc/finit.d/available/webif.conf
+	ln -sf ../available/webif.conf $(FINIT_D)/enabled/webif.conf
+endef
+
+WEBIF_POST_INSTALL_TARGET_HOOKS += WEBIF_INSTALL_FINIT_SVC
+
+$(eval $(generic-package))
---
+++ src/webif/README.md
@@ -0,0 +1,7 @@
+Imported from Buildroot gf5e5aeb where it was removed.
+
+Originally from https://svn.openwrt.org/openwrt/tags/whiterussian_0.9/package/webif
+
+The code is license under the GNU General Public License Version 2.
+
+
