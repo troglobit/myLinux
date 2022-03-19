d5138df29ca5225fff5ba8cc87d1111c52667e87
Joachim Wiberg <troglobit@gmail.com>
2022-03-19 09:07:08 +0100

    package/mcjoin: bump version
    ChangeLog
    
      - Project CI moved from Travis-CI to GitHub
      - Container support: docker pull ghcr.io/troglobit/mcjoin:latest
      - Minor bug fixes, found by Coverity Scan
      - Fix #18: optional trailing group port arguments ip4/ip6, by Adam Young
      - Fix #19: setsid() problem causing mcjoin to exit when daemonized,
        regression introduced in v2.7. Found and fixed by Foster Snowhill
      - Fix #23: fix uptime display in old output mode, by Foster Snowhill
      - Document alternative use-case, by Torstein Eide
    
    Signed-off-by: Joachim Wiberg <troglobit@gmail.com>

--- package/mcjoin/mcjoin.mk
+++ package/mcjoin/mcjoin.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-MCJOIN_VERSION = 2.9
+MCJOIN_VERSION = 2.10
 MCJOIN_SITE    = https://github.com/troglobit/mcjoin/releases/download/v$(MCJOIN_VERSION)
 MCJOIN_LICENSE = ISC
 MCJOIN_LICENSE_FILES = LICENSE
