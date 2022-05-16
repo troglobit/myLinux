16b71aed23f4526daf0647789cbbc7b2ee198ee9
Joachim Wiberg <troglobit@gmail.com>
2022-05-16 06:38:16 +0200

    package/finit: bump to v4.3
    > Critical bug fixes to building for 32-bit targets w/ GLIBC >=2.34
    
    Lots of new features and fixes, both big and small, see the upstream
    ChangeLog: https://github.com/troglobit/finit/releases/tag/4.3
    
    Signed-off-by: Joachim Wiberg <troglobit@gmail.com>

--- package/finit/finit.mk
+++ package/finit/finit.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-FINIT_VERSION = 4.3-rc2
+FINIT_VERSION = 4.3
 FINIT_SITE = https://github.com/troglobit/finit/releases/download/$(FINIT_VERSION)
 FINIT_LICENSE = MIT
 FINIT_LICENSE_FILES = LICENSE
