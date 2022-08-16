1ee0aa338a0bc586d2e04073187986e63afe229c
Joachim Wiberg <troglobit@gmail.com>
2022-08-16 14:07:24 +0200

    package/finit-plugins: menuconfig support for selecting plugins
    Signed-off-by: Joachim Wiberg <troglobit@gmail.com>

--- package/finit-plugins/finit-plugins.mk
+++ package/finit-plugins/finit-plugins.mk
@@ -16,4 +16,16 @@ FINIT_PLUGINS_VERSION = f93132f564dbec1a2285bc8fa782a33906e9cb6d
 FINIT_PLUGINS_AUTORECONF = YES
 FINIT_PLUGINS_DEPENDENCIES += host-automake host-autoconf host-libtool
 
+ifeq ($(BR2_PACKAGE_FINIT_PLUGINS_CHRONY),y)
+FINIT_PLUGINS_CONF_OPTS += --enable-chrony-plugin
+else
+FINIT_PLUGINS_CONF_OPTS += --disable-chrony-plugin
+endif
+
+ifeq ($(BR2_PACKAGE_FINIT_PLUGINS_QMI_PROXY),y)
+FINIT_PLUGINS_CONF_OPTS += --enable-qmi-proxy-plugin
+else
+FINIT_PLUGINS_CONF_OPTS += --disable-qmi-proxy-plugin
+endif
+
 $(eval $(autotools-package))
