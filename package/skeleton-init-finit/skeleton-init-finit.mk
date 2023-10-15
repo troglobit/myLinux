c806a77525103127debbae8bae27cb7e019e28e8
Joachim Wiberg <troglobit@gmail.com>
2023-10-15 15:05:41 +0200

    pacakge/skeleton-init-finit: sync with Infix
     - Keep create tmpfile in top .mk for adding tty to getty.conf
     - Fix sysklogd.conf to run also on mdev systems
    
    Signed-off-by: Joachim Wiberg <troglobit@gmail.com>

--- package/skeleton-init-finit/skeleton-init-finit.mk
+++ package/skeleton-init-finit/skeleton-init-finit.mk
@@ -12,7 +12,7 @@ SKELETON_INIT_FINIT_ADD_TOOLCHAIN_DEPENDENCY = NO
 SKELETON_INIT_FINIT_ADD_SKELETON_DEPENDENCY = NO
 SKELETON_INIT_FINIT_TMPFILE := $(shell mktemp)
 SKELETON_INIT_FINIT_DEPENDENCIES = skeleton-init-common
-
+SKELETON_INIT_FINIT_AVAILABLE = $(SKELETON_INIT_FINIT_PKGDIR)/skeleton/etc/finit.d/available
 # Enable when BR2_INIT_FINT
 #SKELETON_INIT_FINIT_PROVIDES = skeleton
 
@@ -22,7 +22,7 @@ define SKELETON_INIT_FINIT_GETTY
 		if [ $(SYSTEM_GETTY_BAUDRATE) -eq 0 ]; then \
 			SYSTEM_GETTY_BAUDRATE=""; \
 		fi; \
-		echo "tty [12345789] $(SYSTEM_GETTY_PORT) $(SYSTEM_GETTY_BAUDRATE) $(SYSTEM_GETTY_TERM) noclear"; \
+		echo "tty [12345789] $(SYSTEM_GETTY_PORT) $(SYSTEM_GETTY_BAUDRATE) $(SYSTEM_GETTY_TERM) noclear passenv"; \
 	else \
 		echo "tty [12345789] /sbin/getty -L $(SYSTEM_GETTY_OPTIONS) $(SYSTEM_GETTY_BAUDRATE) $(SYSTEM_GETTY_PORT) $(SYSTEM_GETTY_TERM)"; \
 	fi
@@ -37,77 +37,222 @@ define SKELETON_INIT_FINIT_SET_GENERIC_GETTY
 endef
 SKELETON_INIT_FINIT_POST_INSTALL_TARGET_HOOKS += SKELETON_INIT_FINIT_SET_GENERIC_GETTY
 
+# Avahi mDNS-SD
+ifeq ($(BR2_PACKAGE_AVAHI_DAEMON),y)
+define SKELETON_INIT_FINIT_SET_AVAHI
+	cp $(SKELETON_INIT_FINIT_AVAILABLE)/avahi.conf $(FINIT_D)/available/
+	cp $(SKELETON_INIT_FINIT_AVAILABLE)/avahi-dnsconfd.conf $(FINIT_D)/available/
+endef
+SKELETON_INIT_FINIT_POST_INSTALL_TARGET_HOOKS += SKELETON_INIT_FINIT_SET_AVAHI
+endif
+
+ifeq ($(BR2_PACKAGE_AVAHI_AUTOIPD),y)
+define SKELETON_INIT_FINIT_SET_AVAHI_AUTOIPD
+	echo "service [2345789] name:zeroconf :%i avahi-autoipd --force-bind --syslog %i -- ZeroConf for %i" \
+		> $(FINIT_D)/available/zeroconf@.conf
+endef
+SKELETON_INIT_FINIT_POST_INSTALL_TARGET_HOOKS += SKELETON_INIT_FINIT_SET_AVAHI_AUTOIPD
+endif
+
+ifeq ($(BR2_PACKAGE_CHRONY),y)
+define SKELETON_INIT_FINIT_SET_CHRONY
+	cp $(SKELETON_INIT_FINIT_AVAILABLE)/chronyd.conf $(FINIT_D)/available/
+endef
+SKELETON_INIT_FINIT_POST_INSTALL_TARGET_HOOKS += SKELETON_INIT_FINIT_SET_CHRONY
+endif
+
+ifeq ($(BR2_PACKAGE_CONNTRACKD),y)
+define SKELETON_INIT_FINIT_SET_CONNTRACKD
+	cp $(SKELETON_INIT_FINIT_AVAILABLE)/conntrackd.conf $(FINIT_D)/available/
+endef
+SKELETON_INIT_FINIT_POST_INSTALL_TARGET_HOOKS += SKELETON_INIT_FINIT_SET_CONNTRACKD
+endif
+
+ifeq ($(BR2_PACKAGE_DNSMASQ),y)
+define SKELETON_INIT_FINIT_SET_DNSMASQ
+	cp $(SKELETON_INIT_FINIT_AVAILABLE)/dnsmasq.conf $(FINIT_D)/available/
+	ln -sf ../available/dnsmasq.conf $(FINIT_D)/enabled/dnsmasq.conf
+endef
+SKELETON_INIT_FINIT_POST_INSTALL_TARGET_HOOKS += SKELETON_INIT_FINIT_SET_DNSMASQ
+endif
+
 # Dropbear SSH
 ifeq ($(BR2_PACKAGE_DROPBEAR),y)
 define SKELETON_INIT_FINIT_SET_DROPBEAR
+	cp $(SKELETON_INIT_FINIT_AVAILABLE)/dropbear.conf $(FINIT_D)/available/
 	ln -sf ../available/dropbear.conf $(FINIT_D)/enabled/dropbear.conf
 endef
 SKELETON_INIT_FINIT_POST_INSTALL_TARGET_HOOKS += SKELETON_INIT_FINIT_SET_DROPBEAR
 endif
 
-# OpenSSH
-ifeq ($(BR2_PACKAGE_OPENSSH),y)
-define SKELETON_INIT_FINIT_SET_OPENSSH
-	ln -sf ../available/sshd.conf $(FINIT_D)/enabled/sshd.conf
+ifeq ($(BR2_PACKAGE_FRR),y)
+define SKELETON_INIT_FINIT_SET_FRR
+	for svc in babeld bfdd bgpd eigrpd isisd ldpd ospfd ospf6d pathd ripd ripng staticd vrrpd zebra; do	\
+		cp $(SKELETON_INIT_FINIT_AVAILABLE)/frr/$$svc.conf $(FINIT_D)/available/$$svc.conf;		\
+	done
+	ln -sf ../available/zebra.conf $(FINIT_D)/enabled/zebra.conf
 endef
-SKELETON_INIT_FINIT_POST_INSTALL_TARGET_HOOKS += SKELETON_INIT_FINIT_SET_OPENSSH
+SKELETON_INIT_FINIT_POST_INSTALL_TARGET_HOOKS += SKELETON_INIT_FINIT_SET_FRR
+endif # BR2_PACKAGE_FRR
+
+ifeq ($(BR2_PACKAGE_INADYN),y)
+define SKELETON_INIT_FINIT_SET_INADYN
+	cp $(SKELETON_INIT_FINIT_AVAILABLE)/inadyn.conf $(FINIT_D)/available/
+endef
+SKELETON_INIT_FINIT_POST_INSTALL_TARGET_HOOKS += SKELETON_INIT_FINIT_SET_INADYN
 endif
 
 ifeq ($(BR2_PACKAGE_LLDPD),y)
 define SKELETON_INIT_FINIT_SET_LLDPD
-	ln -sf ../available/lldpd.conf $(FINIT_D)/enabled/lldpd.conf
+	cp $(SKELETON_INIT_FINIT_AVAILABLE)/lldpd.conf $(FINIT_D)/available/
 endef
 SKELETON_INIT_FINIT_POST_INSTALL_TARGET_HOOKS += SKELETON_INIT_FINIT_SET_LLDPD
 endif
 
+ifeq ($(BR2_PACKAGE_MSTPD),y)
+define SKELETON_INIT_FINIT_SET_MSTPD
+	cp $(SKELETON_INIT_FINIT_AVAILABLE)/mstpd.conf $(FINIT_D)/available/
+endef
+SKELETON_INIT_FINIT_POST_INSTALL_TARGET_HOOKS += SKELETON_INIT_FINIT_SET_MSTPD
+endif
+
+ifeq ($(BR2_PACKAGE_NETSNMP),y)
+define SKELETON_INIT_FINIT_SET_NETSNMP
+	cp $(SKELETON_INIT_FINIT_AVAILABLE)/snmpd.conf $(FINIT_D)/available/
+endef
+SKELETON_INIT_FINIT_POST_INSTALL_TARGET_HOOKS += SKELETON_INIT_FINIT_SET_NETSNMP
+endif
+
+ifeq ($(BR2_PACKAGE_NGINX),y)
+define SKELETON_INIT_FINIT_SET_NGINX
+	cp $(SKELETON_INIT_FINIT_AVAILABLE)/nginx.conf $(FINIT_D)/available/
+endef
+SKELETON_INIT_FINIT_POST_INSTALL_TARGET_HOOKS += SKELETON_INIT_FINIT_SET_NGINX
+endif
+
+ifeq ($(BR2_PACKAGE_NTPD),y)
+define SKELETON_INIT_FINIT_SET_NTPD
+	cp $(SKELETON_INIT_FINIT_AVAILABLE)/ntpd.conf $(FINIT_D)/available/
+	ln -sf ../available/ntpd.conf $(FINIT_D)/enabled/ntpd.conf
+endef
+SKELETON_INIT_FINIT_POST_INSTALL_TARGET_HOOKS += SKELETON_INIT_FINIT_SET_NTPD
+endif
+
 ifeq ($(BR2_PACKAGE_MINI_SNMPD),y)
 define SKELETON_INIT_FINIT_SET_MINI_SNMPD
+	cp $(SKELETON_INIT_FINIT_AVAILABLE)/mini-snmpd.conf $(FINIT_D)/available/
 	ln -sf ../available/mini-snmpd.conf $(FINIT_D)/enabled/mini-snmpd.conf
 endef
 SKELETON_INIT_FINIT_POST_INSTALL_TARGET_HOOKS += SKELETON_INIT_FINIT_SET_MINI_SNMPD
 endif
 
+# OpenSSH
+ifeq ($(BR2_PACKAGE_OPENSSH),y)
+define SKELETON_INIT_FINIT_SET_OPENSSH
+	cp $(SKELETON_INIT_FINIT_AVAILABLE)/sshd.conf $(FINIT_D)/available/
+	ln -sf ../available/sshd.conf $(FINIT_D)/enabled/sshd.conf
+endef
+SKELETON_INIT_FINIT_POST_INSTALL_TARGET_HOOKS += SKELETON_INIT_FINIT_SET_OPENSSH
+endif
+
+ifeq ($(BR2_PACKAGE_QUAGGA),y)
+define SKELETON_INIT_FINIT_SET_QUAGGA
+	cp $(SKELETON_INIT_FINIT_AVAILABLE)/quagga/zebra.conf $(FINIT_D)/available/
+	ln -sf ../available/zebra.conf $(FINIT_D)/enabled/zebra.conf
+endef
+SKELETON_INIT_FINIT_POST_INSTALL_TARGET_HOOKS += SKELETON_INIT_FINIT_SET_QUAGGA
+
+ifeq ($(BR2_PACKAGE_QUAGGA_ISISD),y)
+define SKELETON_INIT_FINIT_SET_QUAGGA_ISISD
+	cp $(SKELETON_INIT_FINIT_AVAILABLE)/quagga/isisd.conf $(FINIT_D)/available/
+	ln -sf ../available/isisd.conf $(FINIT_D)/enabled/isisd.conf
+endef
+SKELETON_INIT_FINIT_POST_INSTALL_TARGET_HOOKS += SKELETON_INIT_FINIT_SET_QUAGGA_ISISD
+endif
+
+ifeq ($(BR2_PACKAGE_QUAGGA_OSPFD),y)
+define SKELETON_INIT_FINIT_SET_QUAGGA_OSPFD
+	cp $(SKELETON_INIT_FINIT_AVAILABLE)/quagga/ospfd.conf $(FINIT_D)/available/
+	ln -sf ../available/ospfd.conf $(FINIT_D)/enabled/ospfd.conf
+endef
+SKELETON_INIT_FINIT_POST_INSTALL_TARGET_HOOKS += SKELETON_INIT_FINIT_SET_QUAGGA_OSPFD
+endif
+
+ifeq ($(BR2_PACKAGE_QUAGGA_OSP6D),y)
+define SKELETON_INIT_FINIT_SET_QUAGGA_OSP6D
+	cp $(SKELETON_INIT_FINIT_AVAILABLE)/quagga/ospf6d.conf $(FINIT_D)/available/
+	ln -sf ../available/ospf6d.conf $(FINIT_D)/enabled/ospf6d.conf
+endef
+SKELETON_INIT_FINIT_POST_INSTALL_TARGET_HOOKS += SKELETON_INIT_FINIT_SET_QUAGGA_OSP6D
+endif
+
+ifeq ($(BR2_PACKAGE_QUAGGA_RIPD),y)
+define SKELETON_INIT_FINIT_SET_QUAGGA_RIPD
+	cp $(SKELETON_INIT_FINIT_AVAILABLE)/quagga/ripd.conf $(FINIT_D)/available/
+	ln -sf ../available/ripd.conf $(FINIT_D)/enabled/ripd.conf
+endef
+SKELETON_INIT_FINIT_POST_INSTALL_TARGET_HOOKS += SKELETON_INIT_FINIT_SET_QUAGGA_RIPD
+endif
+
+ifeq ($(BR2_PACKAGE_QUAGGA_RIPNGD),y)
+define SKELETON_INIT_FINIT_SET_QUAGGA_RIPNG
+	cp $(SKELETON_INIT_FINIT_AVAILABLE)/quagga/ripng.conf $(FINIT_D)/available/
+	ln -sf ../available/ripng.conf $(FINIT_D)/enabled/ripng.conf
+endef
+SKELETON_INIT_FINIT_POST_INSTALL_TARGET_HOOKS += SKELETON_INIT_FINIT_SET_QUAGGA_RIPNG
+endif
+
+endif # BR2_PACKAGE_QUAGGA
+
+ifeq ($(BR2_PACKAGE_SMCROUTE),y)
+define SKELETON_INIT_FINIT_SET_SMCROUTE
+	cp $(SKELETON_INIT_FINIT_AVAILABLE)/smcroute.conf $(FINIT_D)/available/
+endef
+SKELETON_INIT_FINIT_POST_INSTALL_TARGET_HOOKS += SKELETON_INIT_FINIT_SET_SMCROUTE
+endif
+
+# SSDP Responder
+ifeq ($(BR2_PACKAGE_SSDP_RESPONDER),y)
+define SKELETON_INIT_FINIT_SET_SSDP_RESPONDER
+	cp $(SKELETON_INIT_FINIT_AVAILABLE)/ssdp-responder.conf $(FINIT_D)/available/
+endef
+SKELETON_INIT_FINIT_POST_INSTALL_TARGET_HOOKS += SKELETON_INIT_FINIT_SET_SSDP_RESPONDER
+endif
+
 # Enable Busybox syslogd unless sysklogd is enabled
 ifeq ($(BR2_PACKAGE_SYSKLOGD),y)
 define SKELETON_INIT_FINIT_SET_SYSLOGD
+	cp $(SKELETON_INIT_FINIT_AVAILABLE)/sysklogd.conf $(FINIT_D)/available/
 	ln -sf ../available/sysklogd.conf $(FINIT_D)/enabled/sysklogd.conf
 	rm -f $(FINIT_D)/enabled/syslogd.conf
 endef
 else
 define SKELETON_INIT_FINIT_SET_SYSLOGD
+	cp $(SKELETON_INIT_FINIT_AVAILABLE)/syslogd.conf $(FINIT_D)/available/
 	ln -sf ../available/syslogd.conf $(FINIT_D)/enabled/syslogd.conf
 	rm -f $(FINIT_D)/enabled/sysklogd.conf
 endef
 endif
 SKELETON_INIT_FINIT_POST_INSTALL_TARGET_HOOKS += SKELETON_INIT_FINIT_SET_SYSLOGD
 
-# SSDP Responder
-ifeq ($(BR2_PACKAGE_SSDP_RESPONDER),y)
-define SKELETON_INIT_FINIT_SET_SSDP_RESPONDER
-	ln -sf ../available/ssdp-responder.conf $(FINIT_D)/enabled/ssdp-responder.conf
+ifeq ($(BR2_PACKAGE_ULOGD),y)
+define SKELETON_INIT_FINIT_SET_ULOGD
+	cp $(SKELETON_INIT_FINIT_AVAILABLE)/ulogd.conf $(FINIT_D)/available/
 endef
-SKELETON_INIT_FINIT_POST_INSTALL_TARGET_HOOKS += SKELETON_INIT_FINIT_SET_SSDP_RESPONDER
+SKELETON_INIT_FINIT_POST_INSTALL_TARGET_HOOKS += SKELETON_INIT_FINIT_SET_ULOGD
 endif
 
-ifeq ($(BR2_PACKAGE_SMCROUTE),y)
-define SKELETON_INIT_FINIT_SET_SMCROUTE
-	ln -sf ../available/smcroute.conf $(FINIT_D)/enabled/smcroute.conf
-endef
-SKELETON_INIT_FINIT_POST_INSTALL_TARGET_HOOKS += SKELETON_INIT_FINIT_SET_SMCROUTE
-endif
-
-# Watchdogd
 ifeq ($(BR2_PACKAGE_WATCHDOGD),y)
 define SKELETON_INIT_FINIT_SET_WATCHDOGD
+	cp $(SKELETON_INIT_FINIT_AVAILABLE)/watchdogd.conf $(FINIT_D)/available/
 	ln -sf ../available/watchdogd.conf $(FINIT_D)/enabled/watchdogd.conf
 endef
 SKELETON_INIT_FINIT_POST_INSTALL_TARGET_HOOKS += SKELETON_INIT_FINIT_SET_WATCHDOGD
 endif
 
-# Enable gdbserver when running in Qemu mode
-ifeq ($(QEMU_GDB),y)
+ifeq ($(BR2_TOOLCHAIN_EXTERNAL_GDB_SERVER_COPY),y)
 define SKELETON_INIT_FINIT_SET_GDBSERVER
-	ln -sf ../available/gdbserver.conf $(FINIT_D)/enabled/gdbserver.conf
+	cp $(SKELETON_INIT_FINIT_AVAILABLE)/gdbserver.conf $(FINIT_D)/available/
 endef
 SKELETON_INIT_FINIT_POST_INSTALL_TARGET_HOOKS += SKELETON_INIT_FINIT_SET_GDBSERVER
 endif
@@ -147,6 +292,11 @@ endif
 
 define SKELETON_INIT_FINIT_INSTALL_TARGET_CMDS
 	$(call SYSTEM_RSYNC,$(SKELETON_INIT_FINIT_PKGDIR)/skeleton,$(TARGET_DIR))
+	mkdir -p $(TARGET_DIR)/etc/finit.d/available
+	mkdir -p $(TARGET_DIR)/etc/finit.d/enabled
+	for svc in getty inetd ntpd telnetd; do \
+		cp $(SKELETON_INIT_FINIT_AVAILABLE)/$$svc.conf $(FINIT_D)/available/; \
+	done
 	mkdir -p $(TARGET_DIR)/home
 	mkdir -p $(TARGET_DIR)/srv
 	mkdir -p $(TARGET_DIR)/var
